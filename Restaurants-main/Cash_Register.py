30c82b2d61624698a6cab0ec8473e42fd50a3825
MikeBuelvas <98917675+MikeBuelvas@users.noreply.github.com>
2022-11-24 02:16:59 -0500

    Update Cash_Register.py

--- Restaurants-main/Cash_Register.py
+++ Restaurants-main/Cash_Register.py
@@ -1,22 +1,42 @@
-import Employee, Invoice, Order, Client
+import pickle
+import Employee,Invoice, Client, Order
 
 class Cash_Register():
     def __init__(
             self,
-            balance: str,
+            balance: float,
             admin: Employee,
-            id_admin: str        
+            id_admin: str,
+            sales: list        
     ) -> None:
         self.balance = balance
         self.admin = admin
         self.id_admin = id_admin
-        self.sales = []
-
+        self.sales = sales
+    
     def gen_invoice(
             self,
             id: int,
             client: Client,
             order: Order
-    ) -> Invoice:
-        invoice = Invoice(id,client,order)
-        self.sales.append(invoice)
+        ) -> Invoice:                    
+            invoice = Invoice(id,client,order)
+            for saucer in invoice.order.saucers:
+                invoice.total += saucer.price
+
+            for drink in invoice.order.drinks:
+                invoice.total += drink.price
+            self.balance += invoice.total
+            self.sales.append(invoice)
+            self.save_bs()
+            return invoice
+
+    def save_bs(self) -> None:
+        archive_balance = open("archive_balance","wb")
+        archive_sales = open("archive_sales","wb")
+        pickle.dump(self.balance, archive_balance)
+        pickle.dump(self.sales, archive_sales)
+        archive_balance.close()
+        archive_sales.close()
+        del(archive_balance)
+        del(archive_sales)
